8eb96e2477896e38a7d77fcacb2275fc32f05155
Deivid Santos <deivid@deividroger.net>
2023-05-20 15:30:06 -0300

    EKS terraform setup

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "prefix" {
+ 
+}
+
+variable "cluster_name" {
+  
+}
+
+variable "retation_days" {
+  
+}
+
+variable "desired_size" {
+  
+}
+
+variable "max_size" {
+     
+}
+
+variable "min_size" {
+     
+}
+
